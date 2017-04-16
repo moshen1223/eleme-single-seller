3136958d963a1a7c05d406a5c56e45a8d9d4ce3e
moshen <sizhiqian@sina.cn>
2017-04-16 23:01:11 +0800

    项目第一次提交

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import goods from 'components/goods/Goods.vue';
+import ratings from 'components/ratings/Ratings.vue';
+import seller from 'components/seller/Seller.vue';
+import './common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+/* eslint-disable no-new */
+
+let app = Vue.extend(App);
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+});
+router.start(app, '#app');
+// router.go('/goods');
